cabb238568e8e215f95ed1e21105bbaf5ef80dc2
manhthang <vumanthang08072004@gmail.com>
2026-06-06 11:57:13 +0700

    xác thực trên backend để phân quyền người dùng

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
